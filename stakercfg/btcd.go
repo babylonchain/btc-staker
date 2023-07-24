323166bb6fb92001d762fdc78660311e6a6bc328
Vitalis Salis <vitsalis@gmail.com>
2023-07-24 08:53:38 +0300

    Update default size

--- stakercfg/btcd.go
+++ stakercfg/btcd.go
@@ -10,7 +10,7 @@ const (
 	defaultBtcdRPCHost        = "127.0.0.1:18334"
 	defaultBtcdRPCUser        = "user"
 	defaultBtcdRPCPass        = "pass"
-	defaultBtcdBlockCacheSize = 100000
+	defaultBtcdBlockCacheSize = 20 * 1024 * 1024 // 20 MB
 )
 
 var (
