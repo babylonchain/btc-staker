5b60b53074c8bc49132b25594248becc23fd6e41
KonradStaniec <konrad.staniec@gmail.com>
2024-05-13 14:55:52 +0200

    Add cli command to build unbonding tx (#152)

--- cmd/stakercli/helpers/flags.go
+++ cmd/stakercli/helpers/flags.go
@@ -3,4 +3,6 @@ package helpers
 const (
 	StakingAmountFlag     = "staking-amount"
 	StakingTimeBlocksFlag = "staking-time"
+	UnbondingFee          = "unbonding-fee"
+	UnbondingTimeFlag     = "unbonding-time"
 )
--- cmd/stakercli/transaction/transactions.go
+++ cmd/stakercli/transaction/transactions.go
@@ -15,7 +15,10 @@ import (
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/btcutil/psbt"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/cometbft/cometbft/libs/os"
 	"github.com/urfave/cli"
 )
@@ -39,6 +42,7 @@ var TransactionCommands = []cli.Command{
 		Subcommands: []cli.Command{
 			checkPhase1StakingTransactionCmd,
 			createPhase1StakingTransactionCmd,
+			createPhase1UnbondingTransactionCmd,
 			createPhase1StakingTransactionFromJsonCmd,
 		},
 	},
@@ -106,8 +110,8 @@ func parseMagicBytesFromHex(magicBytesHex string) ([]byte, error) {
 	return magicBytes, nil
 }
 
-func parseStakingAmountFromCliCtx(ctx *cli.Context) (btcutil.Amount, error) {
-	amt := ctx.Int64(helpers.StakingAmountFlag)
+func parseAmountFromCliCtx(ctx *cli.Context, flagName string) (btcutil.Amount, error) {
+	amt := ctx.Int64(flagName)
 
 	if amt <= 0 {
 		return 0, fmt.Errorf("staking amount should be greater than 0")
@@ -116,8 +120,8 @@ func parseStakingAmountFromCliCtx(ctx *cli.Context) (btcutil.Amount, error) {
 	return btcutil.Amount(amt), nil
 }
 
-func parseStakingTimeBlocksFromCliCtx(ctx *cli.Context) (uint16, error) {
-	timeBlocks := ctx.Int64(helpers.StakingTimeBlocksFlag)
+func parseLockTimeBlocksFromCliCtx(ctx *cli.Context, flagName string) (uint16, error) {
+	timeBlocks := ctx.Int64(flagName)
 
 	if timeBlocks <= 0 {
 		return 0, fmt.Errorf("staking time blocks should be greater than 0")
@@ -336,13 +340,13 @@ func createPhase1StakingTransaction(ctx *cli.Context) error {
 		return err
 	}
 
-	stakingAmount, err := parseStakingAmountFromCliCtx(ctx)
+	stakingAmount, err := parseAmountFromCliCtx(ctx, helpers.StakingAmountFlag)
 
 	if err != nil {
 		return err
 	}
 
-	stakingTimeBlocks, err := parseStakingTimeBlocksFromCliCtx(ctx)
+	stakingTimeBlocks, err := parseLockTimeBlocksFromCliCtx(ctx, helpers.StakingTimeBlocksFlag)
 
 	if err != nil {
 		return err
@@ -442,4 +446,219 @@ func MakeCreatePhase1StakingTxResponse(
 	return &CreatePhase1StakingTxResponse{
 		StakingTxHex: hex.EncodeToString(serializedTx),
 	}, nil
+}
+
+// createPhase1UnbondingTransactionCmd creates un-signed unbonding transaction based on
+// provided valid phase1 staking transaction.
+var createPhase1UnbondingTransactionCmd = cli.Command{
+	Name:      "create-phase1-unbonding-transaction",
+	ShortName: "crput",
+	Usage:     "Creates unsigned phase 1 unbonding transaction",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:     stakingTransactionFlag,
+			Usage:    "hex encoded staking transaction for which unbonding transaction will be created",
+			Required: true,
+		},
+		cli.Int64Flag{
+			Name:     helpers.UnbondingFee,
+			Usage:    "unbonding fee in satoshis",
+			Required: true,
+		},
+		cli.Int64Flag{
+			Name:     helpers.UnbondingTimeFlag,
+			Usage:    "Unbonding time in BTC blocks",
+			Required: true,
+		},
+		cli.StringFlag{
+			Name:     magicBytesFlag,
+			Usage:    "Hex encoded magic bytes in staking transaction op return output",
+			Required: true,
+		},
+		cli.StringSliceFlag{
+			Name:     covenantMembersPksFlag,
+			Usage:    "BTC public keys of the covenant committee members",
+			Required: true,
+		},
+		cli.Uint64Flag{
+			Name:     covenantQuorumFlag,
+			Usage:    "Required quorum for the covenant members",
+			Required: true,
+		},
+		cli.StringFlag{
+			Name:     networkNameFlag,
+			Usage:    "Bitcoin network on which staking should take place one of (mainnet, testnet3, regtest, simnet, signet)",
+			Required: true,
+		},
+	},
+	Action: createPhase1UnbondingTransaction,
+}
+
+type CreatePhase1UnbondingTxResponse struct {
+	// bare hex of created unbonding transaction
+	UnbondingTxHex string `json:"unbonding_tx_hex"`
+	// base64 encoded psbt packet which can be used to sign the transaction using
+	// staker bitcoind wallet using `walletprocesspsbt` rpc call
+	UnbondingPsbtPacketBase64 string `json:"unbonding_psbt_packet_base64"`
+}
+
+func createPhase1UnbondingTransaction(ctx *cli.Context) error {
+	net := ctx.String(networkNameFlag)
+
+	currentParams, err := utils.GetBtcNetworkParams(net)
+
+	if err != nil {
+		return err
+	}
+
+	stakingTxHex := ctx.String(stakingTransactionFlag)
+
+	stakingTx, _, err := bbn.NewBTCTxFromHex(stakingTxHex)
+
+	if err != nil {
+		return err
+	}
+
+	magicBytes, err := parseMagicBytesFromCliCtx(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	covenantMembersPks, err := parseCovenantKeysFromCliCtx(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	covenantQuorum := uint32(ctx.Uint64(covenantQuorumFlag))
+
+	stakingTxInfo, err := btcstaking.ParseV0StakingTx(
+		stakingTx,
+		magicBytes,
+		covenantMembersPks,
+		covenantQuorum,
+		currentParams,
+	)
+
+	if err != nil {
+		return fmt.Errorf("invalid staking transaction: %w", err)
+	}
+
+	unbondingFee, err := parseAmountFromCliCtx(ctx, helpers.UnbondingFee)
+
+	if err != nil {
+		return err
+	}
+
+	unbondingTimeBlocks, err := parseLockTimeBlocksFromCliCtx(ctx, helpers.UnbondingTimeFlag)
+
+	if err != nil {
+		return err
+	}
+
+	unbondingAmout := stakingTxInfo.StakingOutput.Value - int64(unbondingFee)
+
+	if unbondingAmout <= 0 {
+		return fmt.Errorf("invalid unbonding amount %d", unbondingAmout)
+	}
+
+	unbondingInfo, err := btcstaking.BuildUnbondingInfo(
+		stakingTxInfo.OpReturnData.StakerPublicKey.PubKey,
+		[]*btcec.PublicKey{stakingTxInfo.OpReturnData.FinalityProviderPublicKey.PubKey},
+		covenantMembersPks,
+		covenantQuorum,
+		unbondingTimeBlocks,
+		btcutil.Amount(unbondingAmout),
+		currentParams,
+	)
+
+	if err != nil {
+		return fmt.Errorf("error building unbonding info: %w", err)
+	}
+
+	stakingTxHash := stakingTx.TxHash()
+	stakingTxInput := wire.NewTxIn(
+		wire.NewOutPoint(
+			&stakingTxHash,
+			uint32(stakingTxInfo.StakingOutputIdx),
+		),
+		nil,
+		nil,
+	)
+
+	unbondingPsbtPacket, err := psbt.New(
+		[]*wire.OutPoint{&stakingTxInput.PreviousOutPoint},
+		[]*wire.TxOut{unbondingInfo.UnbondingOutput},
+		2,
+		0,
+		[]uint32{wire.MaxTxInSequenceNum},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	// re-build staking scripts to properly fill data necessary for signing
+	// in psbt packet
+	stakingScriptInfo, err := btcstaking.BuildStakingInfo(
+		stakingTxInfo.OpReturnData.StakerPublicKey.PubKey,
+		[]*btcec.PublicKey{stakingTxInfo.OpReturnData.FinalityProviderPublicKey.PubKey},
+		covenantMembersPks,
+		covenantQuorum,
+		stakingTxInfo.OpReturnData.StakingTime,
+		btcutil.Amount(stakingTxInfo.StakingOutput.Value),
+		currentParams,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	unbondingPathInfo, err := stakingScriptInfo.UnbondingPathSpendInfo()
+
+	if err != nil {
+		return err
+	}
+
+	unbondingPathCtrlBlock, err := unbondingPathInfo.ControlBlock.ToBytes()
+
+	if err != nil {
+		return err
+	}
+
+	// Fill psbt packet with data which will make it possible for staker to sign
+	// it using his bitcoind wallet
+	unbondingPsbtPacket.Inputs[0].SighashType = txscript.SigHashDefault
+	unbondingPsbtPacket.Inputs[0].WitnessUtxo = stakingTxInfo.StakingOutput
+	unbondingPsbtPacket.Inputs[0].TaprootBip32Derivation = []*psbt.TaprootBip32Derivation{
+		{
+			XOnlyPubKey: stakingTxInfo.OpReturnData.StakerPublicKey.Marshall(),
+		},
+	}
+	unbondingPsbtPacket.Inputs[0].TaprootLeafScript = []*psbt.TaprootTapLeafScript{
+		{
+			ControlBlock: unbondingPathCtrlBlock,
+			Script:       unbondingPathInfo.RevealedLeaf.Script,
+			LeafVersion:  unbondingPathInfo.RevealedLeaf.LeafVersion,
+		},
+	}
+
+	unbondingTxBytes, err := utils.SerializeBtcTransaction(unbondingPsbtPacket.UnsignedTx)
+	if err != nil {
+		return err
+	}
+
+	unbondingPacketEncoded, err := unbondingPsbtPacket.B64Encode()
+
+	if err != nil {
+		return err
+	}
+
+	resp := &CreatePhase1UnbondingTxResponse{
+		UnbondingTxHex:            hex.EncodeToString(unbondingTxBytes),
+		UnbondingPsbtPacketBase64: unbondingPacketEncoded,
+	}
+	helpers.PrintRespJSON(resp)
+	return nil
 }
